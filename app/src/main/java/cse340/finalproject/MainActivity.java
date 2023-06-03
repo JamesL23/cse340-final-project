7b29eae6ac1825ba6c3da6f4a92cd4fe5bb67ad8
James Liu <dragnj@cs.washington.edu>
2023-06-03 13:57:21 -0700

    Display today's date on main screen

--- app/src/main/java/cse340/finalproject/MainActivity.java
+++ app/src/main/java/cse340/finalproject/MainActivity.java
@@ -23,6 +23,8 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -106,12 +108,12 @@ public class MainActivity extends AppCompatActivity {
         }
 
         // https://www.tutorialkart.com/java/how-to-get-current-date-in-yyyy-mm-dd-format-in-java/#gsc.tab=0
-//        LocalDate dateObj = LocalDate.now();
-//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-//        String date = dateObj.format(formatter);
+        LocalDate dateObj = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        String date = dateObj.format(formatter);
 
         Button currDate = findViewById(R.id.main_screen_current_date);
-        currDate.setText("TODAY"); // TODO: REPLACE WITH ACTUAL CURRENT DATE
+        currDate.setText(date);
         // There's a good chance the calendar stuff never gets featured and we only end up
         // saving and editing one workout session because I really am running out of time
         LinearLayout cardContainer = findViewById(R.id.exercise_cards_container);
