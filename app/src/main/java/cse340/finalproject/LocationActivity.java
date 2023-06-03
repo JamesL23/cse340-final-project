113527e53298ce9b96362daef1a635f2588863f3
James Liu <dragnj@cs.washington.edu>
2023-06-03 00:27:40 -0700

    Location screen layout progress

--- app/src/main/java/cse340/finalproject/LocationActivity.java
+++ app/src/main/java/cse340/finalproject/LocationActivity.java
@@ -1,11 +1,16 @@
 package cse340.finalproject;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.mapbox.maps.MapView;
-import com.mapbox.maps.Style;
 
 /**
  * Allows user to define a geofence where they can be reminded to track a workout
@@ -19,7 +24,27 @@ public class LocationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.location_activity);
 
+        TextView title = findViewById(R.id.back_button_top_bar_text);
+        title.setText(getResources().getString(R.string.location_screen_title));
+
+        // info icon displays popup with information on the (unimplemented) geofencing feature
+        ImageView info = findViewById(R.id.info_icon);
+        info.setOnClickListener(view -> {
+                    AlertDialog.Builder alertBuilder = new AlertDialog.Builder(view.getContext());
+                    alertBuilder.setMessage(view.getContext().getResources()
+                            .getString(R.string.location_screen_description));
+                    alertBuilder.setPositiveButton(
+                            "OK",
+                            (dialog, id) -> {
+                                dialog.cancel();
+                                Toast.makeText(view.getContext(), R.string.unimplemented,
+                                        Toast.LENGTH_SHORT).show();
+                            });
+                    alertBuilder.create().show();
+                });
+
         MapView mapView = findViewById(R.id.map_view);
+
 
     }
 }
